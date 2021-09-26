339152dcceb97287b2cf3d468da4adeace700c3b
naviera <jirojas5@uc.cl>
2021-09-26 17:28:04 -0300

    fix: board map populate rubocop fix

--- Rakefile
+++ Rakefile
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'rubocop/rake_task'
+
 task default: %w[test]
 
 task :test do
@@ -7,4 +9,9 @@ task :test do
   ruby 'test/controller_tests.rb'
   ruby 'test/square_tests.rb'
   ruby 'test/board_map_tests.rb'
+end
+
+RuboCop::RakeTask.new(:lint) do |task|
+  task.patterns = ['src/**/*.rb', 'test/**/*.rb']
+  task.fail_on_error = false
 end
--- test/board_map_tests.rb
+++ test/board_map_tests.rb
@@ -4,16 +4,16 @@ require_relative 'test_helper'
 require_relative '../src/model'
 require 'test/unit'
 
-class BoardModelCoordinatesTest < Test::Unit::TestCase
+class BoardMapTest < Test::Unit::TestCase
   def setup
     @mapclass = BoardMap.new
+    @mapclass.map = []
   end
 
   def test_populate
-    @mapclass.map = []
     @mapclass.populate_map
     assert_equal('B', @mapclass.map[0][0].item)
-    assert_equal('?', @mapclass.map[0][0].item_view)
+    assert_equal(false, @mapclass.map[0][0].visible)
     assert_equal(false, @mapclass.map[0][0].flagged)
   end
 end
